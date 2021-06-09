d505cf13b4dd4874fcb1e7fe636e05f413320d44
teekize <elvisteeka1@gmail.com>
2021-06-09 23:37:34 +0300

    created a homepage to house all the featured content

---
+++ src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import "./Home.css";
+import FeaturedInfo from "../../components/featuredInfo/featuredInfo";
+
+const Home = () => {
+
+    return (
+
+        <div className="home">
+
+            <FeaturedInfo />
+        </div>
+    )
+}
+
+export default Home;
