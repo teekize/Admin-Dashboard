ba4ea468681d7beb978c74603c5a70192873b966
teekize <elvisteeka1@gmail.com>
2021-06-10 00:38:23 +0300

    created dummydata for charts component

---
+++ src/dummyData.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+        name: 'Jan',
+        "Active users": 1500,
+
+    },
+    {
+        name: 'Feb',
+        "Active users": 1800,
+
+    },
+    {
+        name: 'Mar',
+        "Active users": 2300,
+
+    },
+    {
+        name: 'April',
+        "Active users": 2800,
+
+    },
+    {
+        name: 'May',
+        "Active users": 4250,
+
+    },
+    {
+        name: 'Jun',
+        "Active users": 4000,
+
+    },
+    {
+        name: 'July',
+        "Active users": 4000,
+
+    },
+    {
+        name: 'Aug',
+        "Active users": 4000,
+
+    },
+    {
+        name: 'Sep',
+        "Active users": 2500,
+
+    },
+    {
+        name: 'Oct',
+        "Active users": 5000,
+
+    },
+    {
+        name: 'Nov',
+        "Active users": 1520,
+
+    },
+    {
+        name: 'Dec',
+        "Active users": 9055,
+
+    }
+
+
+
+];
