7036d3231b960c74c9d8e7851f249a2362bf23c5
teekize <elvisteeka1@gmail.com>
2021-06-10 09:52:22 +0300

    made styling changes

--- src/dummyData.js
+++ src/dummyData.js
@@ -63,3 +63,71 @@ export const data = [
 
 
 ];
+
+export const UserRows = [
+    {
+        id: 1,
+        userName: "John gathuita",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "jon@mail.com",
+        status: "active",
+        transaction: "ksh. 1200"
+    },
+    {
+        id: 2,
+        userName: "Kennedy Muia",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "ken@mail.com",
+        status: "active",
+        transaction: "ksh. 2000"
+    },
+    {
+        id: 3,
+        userName: "Kevin Saiyalel",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "kev@mail.com",
+        status: "active",
+        transaction: "ksh. 800"
+    },
+    {
+        id: 4,
+        userName: "Brandon Leiyan",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "brandon@mail.com",
+        status: "active",
+        transaction: "ksh. 2500"
+    },
+    {
+        id: 5,
+        userName: "Brandon Leiyan",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "brandon@mail.com",
+        status: "active",
+        transaction: "ksh. 2500"
+    },
+    {
+        id: 6,
+        userName: "Brandon Leiyan",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "brandon@mail.com",
+        status: "active",
+        transaction: "ksh. 2500"
+    },
+    {
+        id: 7,
+        userName: "Brandon Leiyan",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "brandon@mail.com",
+        status: "active",
+        transaction: "ksh. 2500"
+    },
+    {
+        id: 8,
+        userName: "Brandon Leiyan",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADea4I5Zmq8Da4GL7w1Bi0NPQTomAbVrc6oMrn_pZiEugw=s32-c-mo",
+        email: "brandon@mail.com",
+        status: "active",
+        transaction: "ksh. 2500"
+    },
+
+];
