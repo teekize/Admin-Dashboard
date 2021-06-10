991612067136385de5bef14225919f779b6b4fe5
teekize <elvisteeka1@gmail.com>
2021-06-10 09:51:42 +0300

    created a router component

--- src/App.js
+++ src/App.js
@@ -2,34 +2,43 @@
 
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
-import Home from "./pages/home/Home"
+import Home from "./pages/home/Home";
+import UserList from "./pages/userlist/UserList";
 import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Topbar />
 
+    <Router>
+      <Topbar />
 
       <div className="container">
 
-
         <Sidebar />
-        <Home />
+
+
+        <Switch>
+
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
 
       </div>
+    </Router>
 
 
 
-
-
-
-
-
-
-
-
-
-    </div>
   );
 }
 
